b9497824fe315e99b5bf95a4ac9259fefd1f2289
Babul Mirdha <babumirdha@gmail.com>
2023-06-01 13:17:52 +0600

    Added security menu

--- resources/views/navigations/admin_nav.blade.php
+++ resources/views/navigations/admin_nav.blade.php
@@ -1,13 +1,13 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="flex items-center shrink-0">
                     <a href="{{ route('admin.dashboard') }}">
                         {{-- Admin --}}
-                        <x-admin-logo class="xblock h-10 w-auto fill-current text-gray-600" />
+                        <x-admin-logo class="w-auto h-10 text-gray-600 fill-current xblock" />
                     </a>
                 </div>
 
@@ -42,11 +42,7 @@
                         </x-nav-link>
                     @endpermission
 
-                    @permission('user_create')
-                        <x-nav-link :href="route('admin.users.index')" :active="request()->routeIs('users.index')">
-                            {{ __('Users') }}
-                        </x-nav-link>
-                    @endpermission
+
 
 
                     @permission('term_create')
@@ -55,17 +51,50 @@
                         </x-nav-link>
                     @endpermission
 
-                    @permission('role_create')
-                        <x-nav-link :href="route('admin.roles.index')" :active="request()->routeIs('roles.index')">
-                            {{ __('Roles') }}
-                        </x-nav-link>
-                    @endpermission
 
-                    @permission('permission_create')
-                        <x-nav-link :href="route('admin.permissions.index')" :active="request()->routeIs('permissions.index')">
-                            {{ __('Permissions') }}
-                        </x-nav-link>
-                    @endpermission
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="flex items-center text-sm font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">
+                                    <div>Security</div>
+
+                                    <div class="ml-1">
+                                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+
+                            <x-slot name="content">
+
+                                @permission('user_create')
+                                    <x-dropdown-link :href="route('admin.users.index')">
+                                        {{ __('Users') }}
+                                    </x-dropdown-link>
+                                @endpermission
+
+
+                                @permission('role_create')
+                                    <x-dropdown-link :href="route('admin.roles.index')">
+                                        {{ __('Roles') }}
+                                    </x-dropdown-link>
+                                @endpermission
+
+                                @permission('permission_create')
+                                    <x-dropdown-link :href="route('admin.permissions.index')">
+                                        {{ __('Permissions') }}
+                                    </x-dropdown-link>
+                                @endpermission
+
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
 
                 </div>
             </div>
@@ -92,11 +121,11 @@
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 <button
-                                    class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                    class="flex items-center text-sm font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300">
                                     <div>{{ Auth::user()->name }}</div>
 
                                     <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 20 20">
                                             <path fill-rule="evenodd"
                                                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -107,11 +136,10 @@
                             </x-slot>
 
 
-
                             <x-slot name="content">
 
                                 <x-dropdown-link :href="route('/')">
-                                    {{ __('Preview') }}
+                                    {{ __('Profile') }}
                                 </x-dropdown-link>
 
                                 <!-- Authentication -->
@@ -134,10 +162,10 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
+            <div class="flex items-center -mr-2 sm:hidden">
                 <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
+                    <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6h16M4 12h16M4 18h16" />
