8873d8e81f63d32d5d309f0280b7cbf8a8894c61
Babul Mirdha <babumirdha@gmail.com>
2023-06-02 21:59:16 +0600

    Refacotord

---
+++ resources/views/Subscription/create.blade.php
@@ -0,0 +1,14 @@
+<x-guest-layout>
+
+
+    @include("Subscription.subscription_form")
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+
+        });
+    </script>
+
+
+</x-guest-layout>
